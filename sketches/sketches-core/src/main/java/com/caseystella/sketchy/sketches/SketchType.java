1640a01b0f4d02dd0c93a3c3cfda19a19433f904
Casey Stella <cestella@gmail.com>
2020-04-17 00:08:31 -0400

    Adding distributional sketch

---
+++ sketches/sketches-core/src/main/java/com/caseystella/sketchy/sketches/SketchType.java
@@ -0,0 +1,14 @@
+package com.caseystella.sketchy.sketches;
+
+import com.esotericsoftware.kryo.KryoSerializable;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+public interface SketchType<S> extends KryoSerializable {
+  S createSketch();
+  void addValue(S sketch, Number value);
+  S merge(S s1, S s2);
+  void serialize(S v, Output output);
+  S materialize(Input input);
+  double getPercentile(S sketch, double pctile);
+}
